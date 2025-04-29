67a12b9d02c37ab796a06f29af83aec30c0e94bd
jiwoo-k <jiwoo000925@naver.com>
2025-04-29 15:10:17 +0900

    첫 base project 생성

---
+++ src/main/java/com/lec/spring/Ai2502TeamProApplication.java
@@ -0,0 +1,13 @@
+package com.lec.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ai2502TeamProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ai2502TeamProApplication.class, args);
+    }
+
+}
