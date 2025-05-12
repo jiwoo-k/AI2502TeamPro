691b331c7749ada43b5da7cbafa5baa06e35d07a
kk523 <kim.hyungoo.i@gmail.com>
2025-05-12 16:38:21 +0900

    팔로우, 신고 로직 구현

--- src/main/java/com/lec/spring/domain/UserFollowing.java
+++ src/main/java/com/lec/spring/domain/UserFollowing.java
@@ -1,15 +1,33 @@
 package com.lec.spring.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class UserFollowing {
-    private Long following_userid;
-    private Long followed_userid;
+
+    private Long followingUserId;
+
+    private Long followedUserId;
+
+    /**
+     * 팔로우하는 사용자 객체.
+     * JSON 변환 시 순환참조를 피하기 위해 @JsonIgnore 처리
+     */
+    @JsonIgnore
+    private User followingUser;
+
+    /**
+     * 팔로우 당하는 사용자 객체.
+     * JSON 변환 시 순환참조를 피하기 위해 @JsonIgnore 처리
+     */
+    @JsonIgnore
+    private User followedUser;
 }
