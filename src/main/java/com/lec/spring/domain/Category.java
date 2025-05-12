2167f46737b77c2f2c1c2cc6164c125c86fbd7fb
AstroXYZ <beomkyu0730@gmail.com>
2025-05-12 10:24:16 +0900

    CREATED - Category.java UPDATED - * jiwoo.erd - 대분류 -> 카테고리 완성 * Tag.java DELETED - .

--- src/main/java/com/lec/spring/domain/Category.java
+++ src/main/java/com/lec/spring/domain/Category.java
@@ -1,4 +1,17 @@
 package com.lec.spring.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Category {
+
+    private Long id;
+    private String name;
+
 }
