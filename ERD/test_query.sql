22530a49d124884d627fd23779537cffa8fa5dc1
jiwoo-k <jiwoo000925@naver.com>
2025-05-13 17:40:04 +0900

    test query 수정

--- ERD/test_query.sql
+++ ERD/test_query.sql
@@ -6,13 +6,30 @@ select t.id, t.category_id,t.name
 from user_tag ut, tag t
 where ut.user_id = 1 and ut.tag_id = t.id;
 
+-- 사용자가 담은 태그와 일치하는 게시글 목록을 들고오려면..
+select p.id, p.user_id, p.type, p.title, p.content, p.createdat, p.isdeleted, p.deletedat
+from post p, post_tag pt
+where p.id=pt.post_id and pt.tag_id in (1, 2, 3, 4);
+
+select c.id, c.name, c.color, count(*)
+from category c, tag t
+where c.id = t.category_id
+group by category_id
+order by count(*) desc;
+
+select c.id "c_id",
+       c.name "c_name",
+       c.color "c_color",
+       count(*) "tagCount"
+from category c, tag t
+where c.id = t.category_id
+group by category_id
+order by tagCount desc;
+
 select * from user_tag where user_id=2;
 select * from post_tag where tag_id=1;
 select * from post;
 select * from post_tag;
 
--- 사용자가 담은 태그와 일치하는 게시글 목록을 들고오려면..
-select p.id, p.user_id, p.type, p.title, p.content, p.createdat, p.isdeleted, p.deletedat
-    from post p, post_tag pt
-where p.id=pt.post_id and pt.tag_id in (1, 2, 3, 4);
+
 
