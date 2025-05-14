0a6f556f5ecd5177f0a0bf1972b419f14bf0a2b8
pract <kimc570612@gmail.com>
2025-05-14 11:10:36 +0900

    최종 수정 (list, detail, wirte, delete) 기능 구현 반영

---
+++ src/main/java/com/lec/spring/vaildator/BoardValidator.java
@@ -0,0 +1,29 @@
+package com.lec.spring.vaildator;
+
+
+import com.lec.spring.domain.Post;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+public class BoardValidator implements Validator {
+
+// 일단 title, content 만 vaildator을 만들었음. 나중에 태그 추가 부분이 생기면 tag == null 부분도 추가해야 함
+    // 또 다른게 있나? vaildator 추가할게 또 뭐가 있지
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Post.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        Post post = (Post) target;
+        if(post.getTitle() == null || post.getTitle().isEmpty()){
+            errors.rejectValue("title", "title","제목을 적으세요");
+        }
+        if (post.getContent() == null || post.getContent().isEmpty()) {
+            errors.rejectValue("content", "content","글을 작성하세요");
+        }
+
+
+    }
+}
