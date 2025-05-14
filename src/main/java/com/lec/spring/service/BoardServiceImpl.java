b2d31fb7a2d453c330a5e0cc729df6d70ee82ec2
pract <kimc570612@gmail.com>
2025-05-14 16:52:38 +0900

    태그 검색 완성

--- src/main/java/com/lec/spring/service/BoardServiceImpl.java
+++ src/main/java/com/lec/spring/service/BoardServiceImpl.java
@@ -1,6 +1,7 @@
 package com.lec.spring.service;
 
 import com.lec.spring.domain.Post;
+import com.lec.spring.domain.Tag;
 import com.lec.spring.repository.PostRepository;
 import com.lec.spring.repository.UserRepository;
 import org.apache.ibatis.session.SqlSession;
@@ -26,8 +27,22 @@ public class BoardServiceImpl implements BoardService {
     @Override
     public Post detail(Long id) {
         System.out.println("잠깐만" + id);
-        return postRepository.findById(id);
+        Post post = postRepository.findById(id);
+        if (post != null) {
+            // post)tag 주입
+            List<Tag> postTag = postRepository.findByPostTag(id);
+            post.setPost_tag(postTag);
+            // user_tag 주입
+            if ("도우미".equals(post.getType())) {
+                List <Tag> usertag = postRepository.findByUserTag(id);
+                System.out.println("usertag" + usertag);
+                post.setUser_tag(usertag);
+
+            }
+        }
+        return post;
     }
+
 
     // 이거 혹시 몰라서 남겨둠
     @Override
@@ -46,7 +61,7 @@ public class BoardServiceImpl implements BoardService {
         return postRepository.update(post);
     }
 
-    
+
     @Override
     public int delete(Long id) {
         int result = 0;
@@ -57,7 +72,7 @@ public class BoardServiceImpl implements BoardService {
         }
         return result;
     }
-// 태그 선택 기능 추가
+    // 태그 선택 기능 추가
     @Override
     public List<Post> listByType(String type) {
         return postRepository.findByType(type);
