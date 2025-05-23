3a7785070b259ed35433574583057ab6531ee780
AstroXYZ <beomkyu0730@gmail.com>
2025-05-23 12:37:55 +0900

    [수정] BoardServiceImpl.java Pagination

--- src/main/java/com/lec/spring/service/BoardServiceImpl.java
+++ src/main/java/com/lec/spring/service/BoardServiceImpl.java
@@ -118,11 +118,67 @@ public class BoardServiceImpl implements BoardService {
         return postRepository.findAll();
     }
 
-    // 페이징 부분
-    @Override
-    public List<Post> list(Integer page, Model model) {
-        return List.of();
-    }
+    // 페이징 부분 (페이징 리스트)
+        // page : 현재 페이지 (1-base)
+        @Override
+        public List<Post> list(Integer page, Model model) {
+            // 현재 페이지 , 디폴트는 1
+            if(page == null) page = 1;
+            if(page < 1) page = 1;
+
+            // 페이징
+            // writePages: 한 [페이징] 당 몇개의 페이지가 표시되나
+            // pageRows: 한 '페이지'에 몇개의 글을 리스트 할것인가?
+            HttpSession session = U.getSession();
+            Integer writePages = (Integer)session.getAttribute("writePages");
+            if(writePages == null) writePages = WRITE_PAGES;   // 만약 session 에 없으면 기본값으로 동작
+            Integer pageRows = (Integer)session.getAttribute("pageRows");
+            if(pageRows == null) pageRows = PAGE_ROWS;   // session 에 없으면 기본값으로
+            session.setAttribute("page", page);   // 현재 페이지 번호 -> session 에 저장
+
+            long cnt = postRepository.countAll(); // 글 목록 전체의 개수
+            int totalPage = (int)Math.ceil(cnt / (double)pageRows);  // 총 몇 '페이지' 분량인가
+
+            // [페이징] 에 표시할 '시작페이지' 와 '마지막페이지'
+            int startPage = 0;
+            int endPage = 0;
+
+            // 해당 '페이지'의 글 목록
+            List<Post> list = null;
+
+            if(cnt > 0){
+                // page 값 보정
+                if(page > totalPage) page = totalPage;
+
+                // 몇번째 데이터부터 fromRow
+                int fromRow = (page - 1) * pageRows;
+
+                // [페이징] 에 표시할 '시작페이지' 와 '마지막페이지' 계산
+                startPage = (((page - 1) / writePages) * writePages) + 1;
+                endPage = startPage + writePages - 1;
+                if (endPage >= totalPage) endPage = totalPage;
+
+                // 해당 page 의 글 목록 읽어오기
+                list = postRepository.selectFromRow(fromRow, pageRows);
+                model.addAttribute("list", list);
+            } else {
+                page = 0;
+            }
+
+            model.addAttribute("cnt", cnt);  // 전체 글 개수
+            model.addAttribute("page", page); // 현재 페이지
+            model.addAttribute("totalPage", totalPage);  // 총 '페이지' 수
+            model.addAttribute("pageRows", pageRows);  // 한 '페이지' 에 표시할 글 개수
+
+            // [페이징]
+            model.addAttribute("url", U.getRequest().getRequestURI());  // 목록 url
+            model.addAttribute("writePages", writePages); // [페이징] 에 표시할 숫자 개수
+            model.addAttribute("startPage", startPage);  // [페이징] 에 표시할 시작 페이지
+            model.addAttribute("endPage", endPage);   // [페이징] 에 표시할 마지막 페이지
+
+
+            return list;
+        }
 
     @Override
     public int update(Post post) {
