5a0ea1b9e1845292ed27d1870a7bb011e1dfd829
jiwoo-k <82378981+jiwoo-k@users.noreply.github.com>
2025-05-27 16:34:24 +0900

    관리자 신고 페이지 폼 다시 수정

--- src/main/resources/static/js/adminUser.js
+++ src/main/resources/static/js/adminUser.js
@@ -6,21 +6,18 @@ $(document).ready(function() {
     $('button.checkPause').click(function() {
        if(confirm('해당 계정을 정지하시겠습니까?')){
            alert('정지되었습니다.');
-           $('form#pause3Form').submit();
        }
     });
 
     $('button.checkBan').click(function() {
         if(confirm('해당 계정을 영구 정지하시겠습니까?')){
             alert('영구 정지되었습니다.');
-            $('form#pause5Form').submit();
         }
     });
 
     $('button.followReset').click(function() {
        if(confirm('해당 계정의 팔로워를 초기화하시겠습니까?')){
            alert('초기화되었습니다.');
-           $('form#banForm').submit();
        }
     });
 });
