c8f77f15f98ded78718869bea2cbbc76f35ecd38
pract <kimc570612@gmail.com>
2025-05-30 11:40:31 +0900

    신고하기 버튼, 신고 경로 수정 완료

--- src/main/java/com/lec/spring/controller/WarningController.java
+++ src/main/java/com/lec/spring/controller/WarningController.java
@@ -1,5 +1,6 @@
 package com.lec.spring.controller;
 
+import com.lec.spring.domain.Post;
 import com.lec.spring.domain.User;
 import com.lec.spring.domain.UserWarning;
 import com.lec.spring.service.UserWarningService;
@@ -20,12 +21,19 @@ public class WarningController {
 
 
     @PostMapping("/warning")
-    public String warning(UserWarning warning, Model model, @AuthenticationPrincipal(expression = "user") User loginUser
+    public String warning(UserWarning warning,
+                          Model model,
+                          @AuthenticationPrincipal(expression = "user") User loginUser
+
     ) {
         warning.setComplaintUserId(loginUser.getId());
+        Post post = warning.getPost();
+        Long postId = warning.getPostId();
+
 
         model.addAttribute("result", warning);
         model.addAttribute("id", loginUser.getId());
+        model.addAttribute("postId", postId);
         System.out.println("warning " + warning);
         userWarningService.report(warning);
         return "board/warning";
